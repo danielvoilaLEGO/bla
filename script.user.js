9d5727ba4bfb39e3a349c2932e482846e7eefae5
Daniel Voila <daniel.voila@consultant.LEGO.com>
2026-05-14 13:35:35 +0300

    dfsfs

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ALH Semi-Auto
 // @namespace    http://tampermonkey.net/
-// @version      3.5
+// @version      3.9
 // @description  Semi-auto flow: searches tickets + selects hour, stops at details for manual fill
 // @match        https://compratickets.alhambra-patronato.es/reservarEntradas.aspx*
 // @grant        GM_xmlhttpRequest
