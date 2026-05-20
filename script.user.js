fd606da8285972a7ee54a69f89034418663c46cd
Daniel Voila <daniel.voila@consultant.LEGO.com>
2026-05-20 20:24:40 +0300

    Update script.user.js

--- script.user.js
+++ script.user.js
@@ -63,7 +63,7 @@
         }
     };
 
-    const LOGIN_EMAIL = "danielvoila@openinbox.io";
+    const LOGIN_EMAIL = "danielvoila03@openinbox.io";
     const LOGIN_PASSWORD = "Teodora1992..92";
 
     const FIREBASE_API_KEY = "AIzaSyBniZTfD3dGs8EzNfqLy956djUwMlCsRYo";
@@ -806,7 +806,7 @@
             });
         });
     }
-    const OPENINBOX_INBOX_ID = "3f372381-8dce-4808-a0e8-88dd62b7d682";
+    const OPENINBOX_INBOX_ID = "17b887f4-356a-456f-9156-02fab50cfad9";
 
     // --- OpenInbox: Poll for latest email ---
     function openinboxPollEmails(inboxId, sentAfter, maxAttempts = 40, intervalMs = 5000) {
